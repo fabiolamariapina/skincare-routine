b6ed628b265c87ffa9c4a4e1d14e30e0c0f9e030
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-15 10:44:57 -0500

    installed mongoose

--- skincare-routine-api/server.js
+++ skincare-routine-api/server.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3003;
 
+// mongoose error/disconnections
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+// mongoose connection
+mongoose.connect("mongodb://localhost:27017/holidays", {
+  useNewUrlParser: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
+
+// controllers
 const skincareControllers = require("./controllers/skincare.js");
 app.use("/skincare", skincareControllers);
 
+// listener
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
 });

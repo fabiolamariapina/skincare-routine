5ee0e8342ece0d764fe215fca72f50fb047244dc
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-15 15:13:38 -0500

    added button from prime react to header to create log in button

--- skincare-routine-front-end/src/components/Header.jsx
+++ skincare-routine-front-end/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import { Button } from "primereact/button";
 
 export default class Header extends Component {
   render() {
     return (
-      <header className="header">
+      <header className="p-d-flex p-jc-between" id="header">
         <h1 className="app-name">skincare routine</h1>
+        <Button
+          label="LOG IN"
+          className="p-button-raised p-button-rounded"
+          id="log-in-button"
+        />
       </header>
     );
   }

13d2e0e8f902a8818062bc6cb9b8f491a929ddf1
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-15 10:42:08 -0500

    added skincare controllers to server.js, confirmed that index route is up and running

--- skincare-routine-api/server.js
+++ skincare-routine-api/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 3003;
 
+const skincareControllers = require("./controllers/skincare.js");
+app.use("/skincare", skincareControllers);
+
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
 });

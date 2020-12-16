219673d0e42100b214df467ea9e964e2532d0f45
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-15 20:16:12 -0500

    added fetch for database call

--- skincare-routine-front-end/src/App.js
+++ skincare-routine-front-end/src/App.js
@@ -3,12 +3,26 @@ import "primeflex/primeflex.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+const baseURL = "http://localhost:3003";
+
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       skincare: [],
     };
+    this.getSkincare = this.getSkincare.bind(this);
+  }
+  getSkincare() {
+    fetch(baseURL + "/your-skincare-routine")
+      .then((data) => {
+        return data.json();
+      })
+      .then((parsedData) => {
+        this.setState({
+          skincare: parsedData,
+        });
+      });
   }
   render() {
     return (

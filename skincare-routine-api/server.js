3cad2a46dc384c2090fbe7dee22a45262d7367a7
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-15 20:33:16 -0500

    added whitelist and corsOptions

--- skincare-routine-api/server.js
+++ skincare-routine-api/server.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express();
 const PORT = 3003;
+
+// middleware
+app.use(express.json());
+
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // mongoose error/disconnections
 mongoose.connection.on("error", (err) =>
@@ -19,9 +35,6 @@ mongoose.connect("mongodb://localhost:27017/holidays", {
 mongoose.connection.once("open", () => {
   console.log("connected to mongoose...");
 });
-
-// middleware
-app.use(express.json());
 
 // controllers
 const skincareControllers = require("./controllers/skincare.js");

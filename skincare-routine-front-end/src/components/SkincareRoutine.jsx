99773912d99d5fa1e06afaf590be8d99b5472653
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-22 19:59:54 -0500

    created skincare routine component, which represents the index page

---
+++ skincare-routine-front-end/src/components/SkincareRoutine.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class SkincareRoutine extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="page-header" id="your-skincare-routine-header">
+          Your Skincare Routine
+        </h3>
+      </div>
+    );
+  }
+}

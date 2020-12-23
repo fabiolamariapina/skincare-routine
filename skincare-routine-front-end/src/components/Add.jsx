f50c9baad29d44ce9e18f7459ae9abf7dcbc2800
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-22 20:39:18 -0500

    removed prime flex since form was not turning out the way I wanted

--- skincare-routine-front-end/src/components/Add.jsx
+++ skincare-routine-front-end/src/components/Add.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
+
 export default class Add extends Component {
   render() {
     return (
-      <div>
+      <div className="main" id="add-main">
         <h3 className="page-header" id="add-to-your-routine-header">
           Add To Your Routine
         </h3>
+        <form>
+          
+        </form>
       </div>
     );
   }
--- skincare-routine-front-end/src/components/SkincareRoutine.jsx
+++ skincare-routine-front-end/src/components/SkincareRoutine.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "primeflex/primeflex.css";
 
 export default class SkincareRoutine extends Component {
   render() {

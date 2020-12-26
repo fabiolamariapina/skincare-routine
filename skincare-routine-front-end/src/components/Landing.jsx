7e6f2e100deaf19b7b7ed208e6d02b2a8db90559
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-26 10:11:24 -0500

    created landing component

---
+++ skincare-routine-front-end/src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="page-header" id="landing-header">
+          Landing Page
+        </h3>
+      </div>
+    );
+  }
+}

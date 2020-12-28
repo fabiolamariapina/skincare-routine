39e1ae5f0cd51206767fb2168429823c2f07df07
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-27 19:25:38 -0500

    created new account component

---
+++ skincare-routine-front-end/src/components/NewAccount.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class NewAccount extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="page-header" id="landing-header">
+          Create A Free Account
+        </h3>
+      </div>
+    );
+  }
+}

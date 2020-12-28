323b9c49e22daecf08d66dfa432321de1e9c179e
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-27 19:36:26 -0500

    added new user form

--- skincare-routine-front-end/src/components/NewAccountForm.jsx
+++ skincare-routine-front-end/src/components/NewAccountForm.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import { Button } from "primereact/button";
 
 export default class NewAccountForm extends Component {
   render() {
     return (
-      <div>
-        <h1>Testing</h1>
+      <div className="new-user-account-form">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <br />
+            <Form.Control type="email" placeholder="Enter email" />
+            <br />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <br />
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <br />
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <br />
+          <Button
+            label="CREATE ACCOUNT"
+            className="p-button-raised p-button-rounded"
+            id="log-in-button"
+            type="submit"
+          />
+        </Form>
       </div>
     );
   }

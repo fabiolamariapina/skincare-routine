8f897cef9699baaa9aad62a927fc64ce7b3b9ffc
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-27 21:22:37 -0500

    added delete function

--- skincare-routine-front-end/src/components/NewForm.jsx
+++ skincare-routine-front-end/src/components/NewForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { Redirect } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 
 const baseURL = "http://localhost:3003";
 
@@ -52,7 +52,7 @@ export default class NewForm extends Component {
         });
       })
       .catch((error) => console.error({ Error: error }));
-    <Redirect to="/your-skincare-routine" />;
+    this.props.history.push("/your-skincare-routine");
   };
   render() {
     return (
